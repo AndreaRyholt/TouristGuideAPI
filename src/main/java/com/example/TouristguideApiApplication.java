44cd0f165dc7d4eea92486333a5a98a167ca709f
AndreaRyholt <anra0003@stud.kea.dk>
2024-02-09 10:28:54 +0100

    Start of project

---
+++ src/main/java/com/example/TouristguideApiApplication.java
@@ -0,0 +1,13 @@
+package tourism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristguideApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TouristguideApiApplication.class, args);
+    }
+
+}

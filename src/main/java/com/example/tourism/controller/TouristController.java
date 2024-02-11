636e51c93a3cdb2ede767088b12998ac6a63d291
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-02-11 19:26:56 +0100

    index

--- src/main/java/com/example/tourism/controller/TouristController.java
+++ src/main/java/com/example/tourism/controller/TouristController.java
@@ -3,16 +3,16 @@ package com.example.tourism.controller;
 import com.example.tourism.model.TouristAttraction;
 import com.example.tourism.repository.TouristRepository;
 import com.example.tourism.service.TouristService;
+import org.apache.logging.log4j.message.Message;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+
 
 @Controller
 @RequestMapping("/attractions") //localhost:8080/attractions
